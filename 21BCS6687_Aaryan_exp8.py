8910c003e70af6f0622c106af5e1aaefcf011031
Aaryan Tyagi <arcaneisc00l@gmail.com>
2024-03-01 16:04:16 +0530

    made more changes to exp 8

--- 21BCS6687_Aaryan_exp8.py
+++ 21BCS6687_Aaryan_exp8.py
@@ -1,6 +1,5 @@
-nums = list(map(int, input('').split(' ')))
+num = list(map(int, input('').split(' ')))
 n = int(input(''))
-count = 0
 
 def getMax(sub, nums):
     if nums == []:
@@ -11,16 +10,23 @@ def getMax(sub, nums):
         nums.remove(max(nums))
         return getMax(sub, nums)
     
-digits = []
-while count < n :
-    sub = n - count
-    if nums == [] or (len(nums) == 1 and sub < nums[0]):
-        break
+max_val = 0
+sorted_nums = sorted(num)[::-1]
+for i in range(len(sorted_nums)):
+    digits = []
+    count = 0
+    nums = sorted_nums[i:]
+    while count < n :
+        sub = n - count
+        if nums == [] or (len(nums) == 1 and sub < nums[0]):
+            break
 
-    max_n = getMax(sub, nums)
-    if max_n is None:
-        break
-    digits.append(max_n)
-    count += max_n
+        max_n = getMax(sub, nums)
+        if max_n is None:
+            break
+        digits.append(max_n)
+        count += max_n
 
-print(sum(digits))
+    max_val = sum(digits) if sum(digits) > max_val else max_val
+
+print(max_val)
